6555a477879cebc5a7990974d3c73dc8b2db4071
Seolcita <Seolcita@gmail.com>
2021-12-02 13:03:28 -0600

    Backend: CRUD for Category

---
+++ backend/routes/category.js
@@ -0,0 +1,25 @@
+/** @format */
+
+const express = require('express');
+const router = express.Router();
+
+// Middlewares
+const { authCheck, adminCheck } = require('../middlewares/auth');
+
+// Controller
+const {
+  create,
+  read,
+  update,
+  remove,
+  list,
+} = require('../controllers/category');
+
+// Routes
+router.post('/category', authCheck, adminCheck, create);
+router.get('/categories', list);
+router.get('/category/:slug', read);
+router.put('/category/:slug', authCheck, adminCheck, update);
+router.delete('/category/:slug', authCheck, adminCheck, remove);
+
+module.exports = router;
