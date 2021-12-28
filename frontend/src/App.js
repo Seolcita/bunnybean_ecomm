2b698db3eaa4c91b17b127b54f39c4c09504f7dc
Seolcita <Seolcita@gmail.com>
2021-12-28 14:02:17 -0600

    Modify - App.js for lazy loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
@@ -8,40 +8,78 @@ import { auth } from './firebase';
 import { useDispatch } from 'react-redux'; //react hook
 import { currentUser } from './connections/auth';
 
-// Pages
-import Navbar from './pages/nav/Navbar';
-import SideDrawer from './pages/drawer/SideDrawer';
-import Home from './pages/Home';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import RegisterComplete from './pages/auth/RegisterComplete';
-import ForgotPassword from './pages/auth/ForgotPassword';
-import OrderHistory from './pages/user/OrderHistory';
-import UserRoute from './components/routes/UserRoute';
-import Account from './pages/user/Account';
-import Wishlist from './pages/user/Wishlist';
-import AdminRoute from './components/routes/AdminRoute';
-import AdminDashboard from './pages/admin/AdminDashboard';
-import Category from './pages/admin/category/Category';
-import CategoryUpdate from './pages/admin/category/CategoryUpdate';
-import SubCategory from './pages/admin/subcategory/SubCategory';
-import SubCategoryUpdate from './pages/admin/subcategory/SubCategoryUpdate';
-import Product from './pages/admin/product/Product';
-import ProductsList from './pages/admin/product/ProductsList';
-import ProductUpdate from './pages/admin/product/ProductUpdate';
-import SingleProduct from './pages/SingleProduct';
-import NewArrivals from './pages/category/NewArrivals';
-import BestSellers from './pages/category/BestSellers';
-import CategoryProducts from './pages/category/CategoryProducts';
-import SubProducts from './pages/subCategory/SubProducts';
-import Shop from './pages/Shop';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Payment from './pages/Payment';
-import Orders from './pages/admin/order/Orders';
-
 // CSS
 import 'react-toastify/dist/ReactToastify.css';
+
+// Pages
+// import Navbar from './pages/nav/Navbar';
+// import SideDrawer from './pages/drawer/SideDrawer';
+// import Home from './pages/Home';
+// import Login from './pages/auth/Login';
+// import Register from './pages/auth/Register';
+// import RegisterComplete from './pages/auth/RegisterComplete';
+// import ForgotPassword from './pages/auth/ForgotPassword';
+// import OrderHistory from './pages/user/OrderHistory';
+// import UserRoute from './components/routes/UserRoute';
+// import Account from './pages/user/Account';
+// import Wishlist from './pages/user/Wishlist';
+// import AdminRoute from './components/routes/AdminRoute';
+// import AdminDashboard from './pages/admin/AdminDashboard';
+// import Category from './pages/admin/category/Category';
+// import CategoryUpdate from './pages/admin/category/CategoryUpdate';
+// import SubCategory from './pages/admin/subcategory/SubCategory';
+// import SubCategoryUpdate from './pages/admin/subcategory/SubCategoryUpdate';
+// import Product from './pages/admin/product/Product';
+// import ProductsList from './pages/admin/product/ProductsList';
+// import ProductUpdate from './pages/admin/product/ProductUpdate';
+// import SingleProduct from './pages/SingleProduct';
+// import NewArrivals from './pages/category/NewArrivals';
+// import BestSellers from './pages/category/BestSellers';
+// import CategoryProducts from './pages/category/CategoryProducts';
+// import SubProducts from './pages/subCategory/SubProducts';
+// import Shop from './pages/Shop';
+// import Cart from './pages/Cart';
+// import Checkout from './pages/Checkout';
+// import Payment from './pages/Payment';
+// import Orders from './pages/admin/order/Orders';
+
+// CODE SPLIT - Load on demand using lazy (except 'SideDrawer')
+import SideDrawer from './pages/drawer/SideDrawer';
+const Navbar = lazy(() => import('./pages/nav/Navbar'));
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import('./pages/auth/Login'));
+const Register = lazy(() => import('./pages/auth/Register'));
+const RegisterComplete = lazy(() => import('./pages/auth/RegisterComplete'));
+const ForgotPassword = lazy(() => import('./pages/auth/ForgotPassword'));
+const OrderHistory = lazy(() => import('./pages/user/OrderHistory'));
+const UserRoute = lazy(() => import('./components/routes/UserRoute'));
+const Account = lazy(() => import('./pages/user/Account'));
+const Wishlist = lazy(() => import('./pages/user/Wishlist'));
+const AdminRoute = lazy(() => import('./components/routes/AdminRoute'));
+const AdminDashboard = lazy(() => import('./pages/admin/AdminDashboard'));
+const Category = lazy(() => import('./pages/admin/category/Category'));
+const CategoryUpdate = lazy(() =>
+  import('./pages/admin/category/CategoryUpdate')
+);
+const SubCategory = lazy(() => import('./pages/admin/subcategory/SubCategory'));
+const SubCategoryUpdate = lazy(() =>
+  import('./pages/admin/subcategory/SubCategoryUpdate')
+);
+const Product = lazy(() => import('./pages/admin/product/Product'));
+const ProductsList = lazy(() => import('./pages/admin/product/ProductsList'));
+const ProductUpdate = lazy(() => import('./pages/admin/product/ProductUpdate'));
+const SingleProduct = lazy(() => import('./pages/SingleProduct'));
+const NewArrivals = lazy(() => import('./pages/category/NewArrivals'));
+const BestSellers = lazy(() => import('./pages/category/BestSellers'));
+const CategoryProducts = lazy(() =>
+  import('./pages/category/CategoryProducts')
+);
+const SubProducts = lazy(() => import('./pages/subCategory/SubProducts'));
+const Shop = lazy(() => import('./pages/Shop'));
+const Cart = lazy(() => import('./pages/Cart'));
+const Checkout = lazy(() => import('./pages/Checkout'));
+const Payment = lazy(() => import('./pages/Payment'));
+const Orders = lazy(() => import('./pages/admin/order/Orders'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -75,7 +113,7 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <>
+    <Suspense fallback='Loading'>
       <Navbar />
       <SideDrawer />
       <ToastContainer />
@@ -121,7 +159,7 @@ const App = () => {
         />
         <AdminRoute exact path='/admin/orders' component={Orders} />
       </Switch>
-    </>
+    </Suspense>
   );
 };
 
